a4c51e74b864407ff04853a34170827a897a5ea3
Peter Horsley <peter.horsley@honeywell.com>
2016-11-30 16:13:32 +1100

    Display edit and delete icons with less opacity to indicate disabled state when sync is active.

---
+++ FileSync/Converters/SyncActiveToIconOpacityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FileSync.Converters
+{
+    using System.Globalization;
+    using System.Windows.Data;
+    public class SyncActiveToIconOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var syncActive = (bool)value;
+            return syncActive ? 0.4 : 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
